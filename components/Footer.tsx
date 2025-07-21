2066556321f2b1c611bd14e84b3f3634428e5306
kalu-Peter <peterkalu41@gmail.com>
2025-07-21 21:26:46 +0300

    footer update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -70,7 +70,7 @@ export default function Footer() {
 
         <div className="border-t border-gray-800 mt-12 pt-8 flex flex-col md:flex-row justify-between items-center">
           <p className="text-gray-400 text-sm">
-            © 2024 Peter Kalu. All rights reserved.
+            © 2025 Peter Kalu. All rights reserved.
           </p>
           <div className="flex space-x-6 mt-4 md:mt-0">
             <Link href="#" className="text-gray-400 hover:text-white text-sm transition-colors cursor-pointer">
